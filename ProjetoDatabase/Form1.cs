7457cfaca272c5f310fa59689612818c787f9c00
unisanta <unisanta@santacecilia.br>
2025-05-12 21:08:11 -0300

    Initial Commit

---
+++ ProjetoDatabase/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjetoDatabase.controller;
+using ProjetoDatabase.model;
+
+
+namespace ProjetoDatabase
+{
+    public partial class Form1 : Form
+    {
+        ClienteController clienteController;
+        //string connectionString = "Server=localhost;Database=db_CLiente;UserId=unisanta;Password=unisanta;";
+        string connectionString = "Server=localhost\\SQLEXPRESS;Database=db_clientes;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            clienteController = new ClienteController(connectionString);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (!clienteController.TestarConexao())
+            {
+                MessageBox.Show("Não foi possível conectar ao banco de dados.");
+                Application.Exit();
+            }
+
+            string nome = textBox1.Text; // Supondo que você tenha um TextBox para o nome
+            Cliente cliente = new Cliente
+            {
+                Name = nome
+            };
+
+            clienteController.AddCliente(cliente);
+            AtualizarClientesGrid(); // Método para atualizar os dados no DataGrid
+        }
+
+        private void AtualizarClientesGrid()
+        {
+            var clientes = clienteController.GetAllClientes();
+            dataGridView1.DataSource = clientes;
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var clientes = clienteController.GetAllClientes();
+            dataGridView1.DataSource = clientes;
+        }
+    }
+
+
+
+
+}
---
+++ ProjetoDatabase/controller/ClienteController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoDatabase.model;
+
+namespace ProjetoDatabase.controller
+{
+    internal class ClienteController
+    {
+        int i = 0;
+        private string connectionString = string.Empty;
+        public ClienteController(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void AddCliente(Cliente c)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO tb_cliente (nome) VALUES (@nome)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@nome", c.Name);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw new Exception(ex.ToString());
+                }
+            }
+        }
+
+        public List<Cliente> GetAllClientes()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            string query = "SELECT id, nome FROM tb_cliente";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Cliente cliente = new Cliente
+                        {
+                            Id = Convert.ToInt32(reader["id"]),
+                            Name = reader["nome"].ToString()
+                        };
+
+                        clientes.Add(cliente);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error fetching clients: " + ex.Message);
+                }
+            }
+
+            return clientes;
+        }
+
+        public bool TestarConexao()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show("Erro de conexão: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+
+
+    }
+}
